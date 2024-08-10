bfd21f02be82d6f7c305acfb83adf40a5d5921ba
Pradeep maurya <mouryapradeep00@gmail.com>
2024-08-10 17:57:18 +0530

    changed as per pr suggestions

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="./assets/vue.svg" class="logo vue change" alt="Vue logo" />
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
@@ -16,7 +16,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <style scoped>
 .logo {
-  height: 6em;
+  height: 72px;
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
